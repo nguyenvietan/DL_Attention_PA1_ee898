54cbc56daf59d0d2b6e16447162f5f43d608075d
nguyen viet an <vietan@ncl-kaist.local>
2019-05-11 10:47:03 +0900

    add run_all_s.sh, run_all_c.sh

---
+++ run_all_c.sh
@@ -0,0 +1,4 @@
+python train.py --arch bam_resnet50_c --epoch 100
+python train.py --arch bam_resnet34_c --epoch 100
+python train.py --arch cbam_resnet50_c --epoch 100
+python train.py --arch cbam_resnet34_c --epoch 100
---
+++ run_all_s.sh
@@ -0,0 +1,4 @@
+python train.py --arch bam_resnet50_s --epoch 100
+python train.py --arch bam_resnet34_s --epoch 100
+python train.py --arch cbam_resnet50_s --epoch 100
+python train.py --arch cbam_resnet34_s --epoch 100
